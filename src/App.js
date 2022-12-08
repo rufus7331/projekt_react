bea7f6f1503fee5bd076df9eb78553922b459808
Hubert <hubertos120@gmail.com>
2022-12-09 00:18:54 +0100

    etap III

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import MovieCard from "./MovieCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=b0d96495bb7ff9a7b9ee1ba364f2b982"
+    const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=b0d96495bb7ff9a7b9ee1ba364f2b982&query="
+
+    const [movies, setMovies] = useState([]);
+    const [term, setTerm] = useState([])
+
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(data => setMovies(data.results))
+    }, [])
+
+
+    console.log(movies)
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+
+        fetch(API_SEARCH + term)
+            .then(res => res.json())
+            .then(data => setMovies(data.results))
+    }
+    return (
+        <div className="App">
+
+            <div className="action_bar">
+
+                <div className="title">
+                    <h1>FilmHub</h1>
+                </div>
+                <div className="input_group_container">
+                    <div className="input-group">
+                        <input type="search" className="form-control" placeholder="Szukaj" aria-label="Szukaj"
+                               aria-describedby="search-addon" onChange={(e) => setTerm(e.target.value)}/>
+                        <button type="button" className="btn btn-warning App-button-search"
+                                onClick={handleSearch}>Szukaj
+                        </button>
+                    </div>
+                </div>
+                <div className="login">
+                    <button class="btn btn-success">Login</button>
+                </div>
+            </div>
+
+
+            <div className="movies">
+                {movies.map((movie) =>
+                    <MovieCard {...movie}/>
+                )}
+            </div>
+<div className="App_footer">
+
+
+            <div className="card w-75">
+                <div className="card-body">
+                    <h5 className="card-title">Wykonano</h5>
+                    <p className="card-text">Copyrights © Hubert Pochroń | 2022</p>
+                    <a href="https://github.com/rufus7331" className="btn btn-primary">Kontakt</a>
+                </div>
+            </div>
+</div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/MovieCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './MovieCard.css'
+
+const MovieCard = (props) => {
+
+    const API_IMG="https://image.tmdb.org/t/p/w500/"
+  return (
+      <div className="movie_card">
+        <div className="poster">
+          <img src={props.poster_path ? API_IMG + props.poster_path : "https://images.unsplash.com/photo-1540224871915-bc8ffb782bdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"} />
+        </div>
+
+        <div className="info">
+          <p className="title">{props.title}</p>
+          <p className="vote">{props.vote_average}</p>
+
+        </div>
+          <div className="overview">
+              <h2 className="title_overview">Overview:</h2>
+              <h3 className="overview_info">{props.overview}</h3>
+          </div>
+      </div>
+  )
+}
+
+export default MovieCard
